4008f2f20c9a02d992627886a514689a99520583
Kaleb Bolack <kalebbolack14@gmail.com>
2023-08-30 17:21:50 -0600

    Passed all test's, was confused about some of the code along and did use a different resource to help!

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+countDown(10);
